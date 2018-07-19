cdc70e2af259b1e4fbf81c3e067cf2401d84bc15
Lawrence <Lawrence4code@gmail.com>
2018-07-19 22:51:45 +0530

    Created HTML structure, added bootstrap with custom styling, JS file added for functionality

---
+++ app/js/data.js
@@ -0,0 +1,378 @@
+var locations = [{
+  title: 'The Silk Route',
+  latitude: 28.524823,
+  longitude: 77.160004,
+  type: 'Restaurant'
+},
+{
+  title: 'Neung Roi - Radisson Blu Plaza',
+  latitude: 28.546511,
+  longitude: 77.119681,
+  type: 'Restaurant'
+},
+{
+  title: 'Mynt Bar & Lounge',
+  latitude: 28.551637,
+  longitude: 77.128040,
+  type: 'Sport and Entertainment'
+},
+{
+  title: 'Moti Mahal Restaurant',
+  latitude: 28.539081,
+  longitude: 77.129292,
+  type: 'Restaurant'
+},
+{
+  title: 'Setz',
+  latitude: 28.543311,
+  longitude: 77.156469,
+  type: 'Restaurant'
+},
+{
+  title: 'NANKING',
+  latitude: 28.536701,
+  longitude: 77.148031,
+  type: 'Restaurant'
+},
+{
+  title: 'Tipu Sultan',
+  latitude: 28.524711,
+  longitude: 77.156841,
+  type: 'Restaurant'
+},
+{
+  title: 'Vasant Square Mall',
+  latitude: 28.524749,
+  longitude: 77.156543,
+  type: 'Shopping Mall'
+},
+{
+  title: 'DLF Promenade',
+  latitude: 28.542654,
+  longitude: 77.155753,
+  type: 'Shopping Mall'
+},
+{
+  title: 'Chattarpur Metro Station',
+  latitude: 28.506932,
+  longitude: 77.175507,
+  type: 'Transportation'
+},
+{
+  title: 'Qutub Minar Metro Station',
+  latitude: 28.512971,
+  longitude: 77.186358,
+  type: 'Transportation'
+},
+{
+  title: 'Fortis Hospital',
+  latitude: 28.519478,
+  longitude: 77.160442,
+  type: 'Hospital'
+},
+{
+  title: 'Indian Spinal Injuries Centre',
+  latitude: 28.533132,
+  longitude: 77.142185,
+  type: 'Hospital'
+},
+{
+  title: 'Delhi Hurricanes Rugby FC',
+  latitude: 28.526859,
+  longitude: 77.159386,
+  type: 'Sport and Entertainment'
+},
+{
+  title: 'G Bar - The Grand',
+  latitude: 28.543823,
+  longitude: 77.152125,
+  type: 'Sport and Entertainment'
+}
+]
+
+
+var styles = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8ec3b9"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1a3646"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#4b6878"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#64779e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.province",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#4b6878"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.man_made",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#334e87"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#283d6a"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#6f9ba5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3C7680"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#304a7d"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#98a5be"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#2c6675"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#255763"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#b0d5ce"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#023e58"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#98a5be"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1d2c4d"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#283d6a"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3a4762"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#0e1626"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#4e6d70"
+      }
+    ]
+  }
+]
--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,100 @@
-console.log('From script.js file');
+function AppViewModel() {
+  var that = this;
+  this.filterOptions = ko.observable("");
+  this.markers = [] // empty array created to store all the markers for the desired locations
 
-// Client ID
-// 4YE2SWVUWIBNRX55TLKO3OQHOIKNQGA20XMGAOEAYB1NH515
-// Client Secret
-// AFLJ0MYEWLX5UTRY4JFLI1YG33B2FSK3ELSWRLHOBNK5WFUD
+  // function to initiate map in the map area
+  this.mapInit = function () {
+    // mapDetails for the basic setting of the map
+    var mapDetails = {
+      center: new google.maps.LatLng(28.5273, 77.1515),
+      zoom: 13,
+      styles: styles, // styles from data.js file
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        position: google.maps.ControlPosition.TOP_CENTER
+      },
+      zoomControl: true,
+      zoomControlOptions: { position: google.maps.ControlPosition.RIGHT_CENTER }
+    }
+    // creating map in the doc with above map settings and targeting map id in the HTML DOC
+    var map = new google.maps.Map(document.getElementById('map'), mapDetails);
+
+    this.infoWindow = new google.maps.InfoWindow();   // creating info window for markers using google API
+
+    for (var i = 0; i < locations.length; i++) { // for loops on the locations array containing marker information
+      this.markerTitle = locations[i].title;
+      this.markerLatitude = locations[i].latitude;
+      this.markerLongitude = locations[i].longitude;
+      this.markerType = locations[i].type;
+
+      this.marker = new google.maps.Marker({
+        map,
+        position: {
+          lat: this.markerLatitude,
+          lng: this.markerLongitude
+        },
+        title: this.markerTitle,
+        lat: this.markerLatitude,
+        lng: this.markerLongitude,
+        animation: google.maps.Animation.DROP //https://developers.google.com/maps/documentation/javascript/examples/marker-animations
+      });
+      this.markers.push(this.marker); // storing all the markers in the this.markers array
+      this.marker.addListener("click", that.popAndBounce); // adding function click for each marker
+    }
+  };
+
+  // function created to pop the window up with location information
+  this.populateInfoWindow = function (marker, infoWindow) {
+    if (infoWindow.marker != marker) {
+      infoWindow.setContent('');
+      infoWindow.marker = marker;
+      var CLIENT_ID = "4YE2SWVUWIBNRX55TLKO3OQHOIKNQGA20XMGAOEAYB1NH515"; // clientID from FourSqaure
+      var CLIENT_SECRET = "AFLJ0MYEWLX5UTRY4JFLI1YG33B2FSK3ELSWRLHOBNK5WFUD"; // clientSecret from FourSqaure
+      // reference: https://stackoverflow.com/questions/3066586/get-string-in-yyyymmdd-format-from-js-date-object
+      var reqDateFormat = new Date().toISOString().substring(0, 10).replace(/-/g, ''); // for api version from FourSquare
+      // reference: https://developer.foursquare.com/docs/api/configuration/versioning
+      var fsURL = "https://api.foursquare.com/v2/venues/search?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&ll=" + marker.lat + "," + marker.lng + "&query=" + marker.title.replace(/ /g, '') + "&v=" + reqDateFormat;
+      $.getJSON(fsURL).done(function (marker) {
+        var response = marker.response.venues[0];
+        that.name = response.name;
+        that.street = response.location.formattedAddress[0];
+        that.city = response.location.formattedAddress[1] || "New Delhi"; // use default in case of non-availability
+        that.country = response.location.formattedAddress[3] || "India"; // use default in case of non-availability
+        that.category = response.categories[0].name;
+        that.windowContent =
+          "<h5>" + that.name + "</h5>" +
+          "<p> Address: " + that.street + "</p>" +
+          "<p>" + that.city + "</p>" +
+          "<p>" + that.country + "</p>";
+        infoWindow.setContent(that.windowContent);
+      }).fail(function () { alert("Opps, some issue with required Web API. Please refresh the page.") })
+      infoWindow.open(map, marker);
+    }
+  };
+  // function created for bounce animaton and included populateInfoWindow
+  this.popAndBounce = function () {
+    that.populateInfoWindow(this, that.infoWindow);
+    this.setAnimation(google.maps.Animation.BOUNCE)
+    setTimeout((function () {
+      this.setAnimation(null);
+    }).bind(this), 1450)
+  };
+
+  this.mapInit();   // intiating the map function
+
+  this.locationsSearch = ko.computed(function () {
+    var searchResult = [];
+    for (var i = 0; i < this.markers.length; i++) {
+      var searchLocation = this.markers[i];
+      if (searchLocation.title.toLowerCase().includes(this.filterOptions().toLowerCase())) {
+        searchResult.push(searchLocation);
+        this.markers[i].setVisible(true);
+      }  // https://stackoverflow.com/questions/38536554/do-i-need-a-last-else-clause-in-an-if-else-if-statement
+    }
+    return searchResult; // returning the search result
+  }, this);
+}
+
+function appInit() { ko.applyBindings(new AppViewModel()); }
